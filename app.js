8c8babd2c2e7e6a4266adfd5cb09281bd7971534
Eddy Caldas <eddycaldas@live.com>
2018-01-06 20:04:25 -0700

    functions

--- app.js
+++ app.js
@@ -5,5 +5,30 @@ const square = function(x=3){
 console.log(square());
 
 
-// Immidiately Invokable functions expressions IIFEs
+// Immidiately Invokable functions expressions IIFEs- declare and run at the same time
+(function(){
+    console.log("IIFE Ran...");
+})();
+                    //--
+(function(name){
+    console.log("Hello " + name);
+})('Brad');
 
+
+// Property methods - when a function inside object, its call method
+const todo = {
+    add: function(){
+        console.log('Add todo...');
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`);
+    }
+}
+
+todo.delete = function() {
+    console.log("Delete todo...");
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
